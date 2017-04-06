fd573285c7e948a339f5a8b6b0350d88153131e3
qzers <pasweb@mail.ru>
2017-04-06 22:08:04 +0300

    Creat ChapterOne

--- ru.pasweb/src/main/java/ChapterOne.java
+++ ru.pasweb/src/main/java/ChapterOne.java
@@ -4,5 +4,12 @@
 public class ChapterOne {
     public static void main(String[] args){
         System.out.println("Chapter One");
+        int x = 1;
+        while (x < 3){
+        System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+        if(x  == 3) System.out.println("Doo");
     }
 }
